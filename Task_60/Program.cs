d0acfb9668e8cce72e40c1155859e68326627bdd
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-30 20:02:43 +0300

    Task_60

---
+++ Task_60/Program.cs
@@ -0,0 +1,37 @@
+﻿void Matrix(int[,,] matrix)
+{
+    int count = 10;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int t = 0; t < matrix.GetLength(2); t++)
+            {
+                matrix[t, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int t = 0; t < matrix.GetLength(2); t++)
+            {
+                Console.Write($"{matrix[i, j, t]}({i},{j},{t}) ");
+            }
+        }
+    }
+}
+
+Console.Clear();
+int[,,] matrix = new int[2, 2, 2];
+Matrix(matrix);
+PrintMatrix(matrix);
+
+
