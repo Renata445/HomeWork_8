343eed1c896f199709b61ccac850ab73357c0abf
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-30 20:02:27 +0300

    Task_56

---
+++ Task_56/Program.cs
@@ -0,0 +1,56 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+        }
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MinSumRow(int[,] matrix)
+{
+    int MinRowSum = 0;
+    int MinRow = 0;
+    int SumRow = 0;
+    for(int i = 0; i < matrix.GetLength(1); i++)
+    {
+        MinRow += matrix[0, i];
+    }
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            SumRow += matrix[i, j];
+        }
+        if (SumRow < MinRow)
+        {
+            MinRow = SumRow;
+            MinRowSum = i;
+        }
+        SumRow = 0;
+    }
+    Console.Write($"{MinRowSum + 1} строка");
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+Console.WriteLine("Начальный массив:");
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+MinSumRow(matrix);
